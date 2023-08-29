46ade20079084eecc9960b3446322deeaef17fc8
Nadzeya Kizer <nadin1kiz@gmail.com>
2023-08-29 20:12:58 +0100

    changed screen resolution

--- code/TestAutomation.Core/Browser/DriverFactory.cs
+++ code/TestAutomation.Core/Browser/DriverFactory.cs
@@ -3,6 +3,7 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Safari;
+using System.Drawing;
 using TestAutomation.Core.Enums;
 using TestAutomation.Core.Utilities;
 using TestAutomation.Utilities;
@@ -43,7 +44,8 @@ namespace TestAutomation.Core.Browser
             chromeOptions.AddArgument("--start-maximized");
             var service = ChromeDriverService.CreateDefaultService();
             var chromeDriver = new ChromeDriver(service, chromeOptions, TimeSpan.FromMinutes(2));
-            chromeDriver.Manage().Window.Maximize();
+            //chromeDriver.Manage().Window.Maximize();
+            chromeDriver.Manage().Window.Size = new Size(1280, 720);
             return chromeDriver;
         }
 
